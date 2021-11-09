ede3ec0f81fa8a92c04f51167b3a3c045bd56807
Matomas <matomas225@gmail.com>
2021-11-09 17:42:21 +0200

    Added Design and clicking functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,89 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  //States
+
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  const [imgSrc, setImgSrc] = useState<string>("");
+  const [border, setBorder] = useState<string>("200px");
+  //Local Storage
+
+  //Functions
+
+  const clickHandle = () => {
+    if (isClicked === false) {
+      setIsClicked(true);
+      setBorder("300px");
+      console.log(border);
+    } else if (isClicked === true) {
+      setIsClicked(false);
+      setBorder("200px");
+      console.log(border);
+    }
+  };
+  const checkHandle = () => {
+    if (isChecked === false) {
+      setIsChecked(true);
+    } else if (isChecked === true) {
+      setIsChecked(false);
+    }
+  };
+
+  //UseEffects
+
+  useEffect(() => {
+    if (isChecked === true) {
+      setImgSrc("./Img/CircleChecked.svg");
+    } else if (isChecked === false) {
+      setImgSrc("./Img/Circle.svg");
+    }
+  }, [isChecked]);
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>CCT Lab Process</h1>
+      <ul>
+        <li>
+          <div className="main-card" onClick={clickHandle}>
+            <h1>1</h1>
+            <p>Build test task</p>
+            <div className="border" style={{ width: border }}></div>
+            <div className="list"></div>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+        </li>
+        <li>
+          <div className="main-card" onClick={clickHandle}>
+            <h1>1</h1>
+            <p>Build test task</p>
+            <div className="border" style={{ width: border }}></div>
+            <div className="list"></div>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+          <div className="side-card">
+            <img src={imgSrc} alt="check-box" onClick={checkHandle} />
+            <p>Create repository</p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./Styles/Styles.css";
+import "./Styles/Style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
